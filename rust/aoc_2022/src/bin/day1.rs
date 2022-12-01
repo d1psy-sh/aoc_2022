118f00f68b70208b2df03bcabda6aec6079a32b1
nkirschall <niki.nice1203@gmail.com>
2022-12-01 22:50:19 +0100

    add(day1): rust problem two

--- rust/aoc_2022/src/bin/day1.rs
+++ rust/aoc_2022/src/bin/day1.rs
@@ -1,6 +1,13 @@
 fn main() {
     let input = include_str!("./input.txt");
     let split = input.split("\n\n").collect::<Vec<&str>>();
+    let res_one = problem_one(split.clone());
+    let res_two = problem_two(split);
+    println!("{:?}", res_one);
+    println!("{:?}", res_two);
+}
+
+fn problem_one(split: Vec<&str>) -> usize {
     let mut res = 0;
     for s in split {
         let tmp: usize = s
@@ -13,6 +20,21 @@ fn main() {
             res = tmp;
         }
     }
+    return res;
+}
 
-    println!("{:?}", res);
+
+fn problem_two(split: Vec<&str>) -> usize {
+    let mut res: Vec<usize> = vec![]; 
+    for s in split {
+        let tmp: usize = s
+            .split("\n")
+            .flat_map(|x| x.parse::<usize>())
+            .collect::<Vec<usize>>()
+            .iter()
+            .sum();
+        res.push(tmp);
+    }
+    res.sort_by(|a, b| b.cmp(a));
+    return res.iter().take(3).sum();
 }
